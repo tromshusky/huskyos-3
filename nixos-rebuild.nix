40e6fd8c9c69db411319d730f9044ace178a5a61
tromshusky <tromshusky@proton.me>
2026-07-28 21:59:19 +0000

    added autoupdate

--- nixos-rebuild.nix
+++ nixos-rebuild.nix
@@ -3,6 +3,7 @@ let
   rb-script = import ./rebuild-boot.sh.nix { inherit pkgs; };
   rb-bin = pkgs.writeScriptBin "nixos-rebuild" rb-script;
 in {
+  system.autoUpgrade.enable = true;
   system.build.nixos-rebuild = lib.mkForce rb-bin;
   system.tools.nixos-rebuild.enable = false;
   environment.systemPackages = [ rb-bin ];
