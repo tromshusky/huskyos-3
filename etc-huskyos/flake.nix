3859f8746779305f792d6e04cab72d309abbed21
tromshusky <tromshusky@proton.me>
2026-08-26 12:59:50 +0000

    Update flake.nix

--- etc-huskyos/flake.nix
+++ etc-huskyos/flake.nix
@@ -1,4 +1,6 @@
 {
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-26.05;
   inputs.huskyos.url = github:tromshusky/huskyos-3;
+  inputs.huskyos.inputs.nixpkgs.follows = "nixpkgs";
   outputs = { huskyos, self, ... }: huskyos.withSelf self;
 }
